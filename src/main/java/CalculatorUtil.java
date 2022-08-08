21f3c8d3abbe55f5a3e7e724738d4268469937fd
tharindud <tharindua@wso2.com>
2022-08-08 23:20:37 +0530

    initial commit

---
+++ src/main/java/CalculatorUtil.java
@@ -0,0 +1,22 @@
+
+public class CalculatorUtil {
+
+    public static long add(long x, long y) {
+        return x + y;
+    }
+
+    public static long subtract(long x, long y) {
+        return x - y;
+    }
+
+    public static long multiply(long x, long y) {
+        return x * y;
+    }
+
+    public static long divide(long x, long y) {
+        return x / y;
+    }
+    public static long abc(long x,long y) {
+        return x / y;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World" );
+        geta();
+    }
+    public static void geta() {
+        System.out.println("Hello World" );
+    }
+}
